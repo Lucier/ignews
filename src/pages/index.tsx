d73edc5998717f7d80d2d74b4b2c47417eac29cb
Lucier Ferreira Lima <lucierflima@gmail.com>
2021-03-23 02:55:36 -0300

    Finalizaçao de parte visual

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,31 @@
+import Head from 'next/head';
+import { SubscribeButton } from '../components/SubscribeButton';
+
+import styles from './home.module.scss';
+
 export default function Home() {
-  return <h1>ignews</h1>;
+  return (
+    <>
+      <Head>
+        <title>Início | ig.News </title>
+      </Head>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>Olá! Seja bem vindo! </span>
+          <h1>
+            Notícias sobre o mundo
+            <span>React. </span>
+          </h1>
+          <p>
+            Acesse todo o nosso conteúdo
+            <br />
+            <span>por apenas R$ 9,90/mês </span>
+          </p>
+          <SubscribeButton />
+        </section>
+
+        <img src="/images/avatar.svg" alt="Estudante" />
+      </main>
+    </>
+  );
 }
